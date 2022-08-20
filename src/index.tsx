81f6ca3777c2592c599a9f420022b1d0dc34179f
Alex Kobylansky <alex.kobylansky@gmail.com>
2022-08-20 22:23:56 +0300

    Import styles changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './App.css';
 import {App} from './App';
-import './assets/css/reset.css'
-import './assets/css/style.css';
+import './reset.css'
 
 ReactDOM.render(
   <React.StrictMode>
